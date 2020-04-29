193285a09b90e8861abdc62f9ed4c28e624091d0
AkJkSenpai <52347928+AkJkSenpai@users.noreply.github.com>
2020-04-29 13:09:17 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,11 +1,52 @@
 <?php
+    $name = $_POST['nimi'];
+    $visitor_email = $_POST['sposti'];
+    $message = $_POST['tarkennus'];
 
-if($_POST["message"]) {
 
-mail("your@email.address", "Here is the subject line",
+    $email_from = 'yourname@yourwebsite.com';
 
-$_POST["insert your message here"]. "From: an@email.address");
+    $email_subject = "New Form submission";
 
+    $email_body = "You have received a new message from the user $name.\n".
+                          "Here is the message:\n $message".
+
+    $to = "aku.paananen100@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+                        
+    $headers .= "Reply-To: $visitor_email \r\n";
+                        
+    mail($to,$email_subject,$email_body,$headers);
+
+    function IsInjected($str)
+{
+    $injections = array('(\n+)',
+           '(\r+)',
+           '(\t+)',
+           '(%0A+)',
+           '(%0D+)',
+           '(%08+)',
+           '(%09+)'
+           );
+               
+    $inject = join('|', $injections);
+    $inject = "/$inject/i";
+    
+    if(preg_match($inject,$str))
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
 }
 
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+  
 ?>
