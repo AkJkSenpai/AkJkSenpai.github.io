687b8c0d32557e95871dcf48c82714f66dd8a7a9
AkJkSenpai <52347928+AkJkSenpai@users.noreply.github.com>
2020-04-29 14:34:34 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,52 +1,20 @@
 <?php
-    $name = $_POST['nimi'];
-    $visitor_email = $_POST['sposti'];
-    $message = $_POST['tarkennus'];
 
+    if(isset($_POST['submit'])){
+        $name = $_POST['nimi'];
+        $email = $_POST['sposti'];
+        $msg = $_POST['tarkennus'];
 
-    $email_from = 'yourname@yourwebsite.com';
+        $to = 'aku.paananen100@gmail.com';
+        $subject = 'Form Submission';
+        $message = "Nimi: " . $name . "\n" . "Tarkennus palveluun: " . "\n" . $msg;
+        $headers = "From: " . $email;
 
-    $email_subject = "New Form submission";
-
-    $email_body = "You have received a new message from the user $name.\n".
-                          "Here is the message:\n $message".
-
-    $to = "aku.paananen100@gmail.com";
-
-    $headers = "From: $email_from \r\n";
-                        
-    $headers .= "Reply-To: $visitor_email \r\n";
-                        
-    mail($to,$email_subject,$email_body,$headers);
-
-    function IsInjected($str)
-{
-    $injections = array('(\n+)',
-           '(\r+)',
-           '(\t+)',
-           '(%0A+)',
-           '(%0D+)',
-           '(%08+)',
-           '(%09+)'
-           );
-               
-    $inject = join('|', $injections);
-    $inject = "/$inject/i";
-    
-    if(preg_match($inject,$str))
-    {
-      return true;
+        if(mail($to, $subject, $message, $headers)){
+            echo "<h1>Lähetys onnistui</h1>";
+        }else{
+            echo "Jotain meni pieleen";
+        }
     }
-    else
-    {
-      return false;
-    }
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
   
 ?>
